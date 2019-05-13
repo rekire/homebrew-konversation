5384a105d8033d26c2a820f9d7fdcc9cb001d453
rekire <git@reki.re>
2019-05-13 06:41:47 +0200

    logging for more info

--- Formula/konversation.rb
+++ Formula/konversation.rb
@@ -5,6 +5,10 @@ class Konversation < Formula
   sha256 "6123d126278faae2419f5de00411a1b67ae57e0cf2265a5d484ed6f9786baaca"
 
   def install
-    system "echo", "Testing env: libexec=#{libexec} bin=#{bin} prefix=#{prefix} pkgshare=#{pkgshare}"
+    system "ls", "-la", "#{libexec}"
+    system "ls", "-la", "#{bin}"
+    system "ls", "-la", "#{prefix}"
+    system "ls", "-la", "#{pkgshare}"
+    #system "echo", "Testing env: libexec=#{libexec} bin=#{bin} prefix=#{prefix} pkgshare=#{pkgshare}"
   end
 end
