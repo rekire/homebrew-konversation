5a08791f1fb1dd5eef8c308a54a6007d69b29f0c
rekire <git@reki.re>
2019-05-15 06:45:42 +0200

    Rollback

--- Formula/konversation.rb
+++ Formula/konversation.rb
@@ -5,7 +5,7 @@ class Konversation < Formula
   sha256 "6123d126278faae2419f5de00411a1b67ae57e0cf2265a5d484ed6f9786baaca"
 
   def install
-    libexec.install Dir["#{buildpath}/*.jar"]
+    libexec.install "#{buildpath}/konversation-cli.jar"
     bin.write_jar_script "#{libexec}/konversation-cli.jar", "konversation"
   end
 end
