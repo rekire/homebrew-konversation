62abe2d07134dcf72e2ee90bd58085fed0ee3a4c
rekire <git@reki.re>
2019-05-15 06:39:55 +0200

    Try using "Dir" instead of a simple string

--- Formula/konversation.rb
+++ Formula/konversation.rb
@@ -5,7 +5,7 @@ class Konversation < Formula
   sha256 "6123d126278faae2419f5de00411a1b67ae57e0cf2265a5d484ed6f9786baaca"
 
   def install
-    libexec.install "#{buildpath}/konversation-cli.jar"
+    libexec.install Dir["#{buildpath}/*.jar"]
     bin.write_jar_script "#{libexec}/konversation-cli.jar", "konversation"
   end
 end
