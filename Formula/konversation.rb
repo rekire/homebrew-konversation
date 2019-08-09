6d7ecc852d720cf5a2faea587a828d21922bdbf1
rekire <git@reki.re>
2019-08-09 10:38:38 +0200

    Undo hash change

--- Formula/konversation.rb
+++ Formula/konversation.rb
@@ -2,7 +2,7 @@ class Konversation < Formula
   desc "Konversation is a tool to generate rich and diversified responses to the user of a voice application."
   homepage "https://github.com/rewe-digital-incubator/Konversation/"
   url "https://github.com/rewe-digital-incubator/Konversation/releases/download/v1.1.0-rc2/konversation-cli.jar"
-  sha256 "e9cb96d656a22fdc680e6d3139111381865d3caddcea419076fc27df0649a8ad"
+  sha256 "d784d55fb5558e2f7c3d564d0cda38ae219fee8598cc7b00775840efd8f199b8"
 
   def install
     libexec.install "#{buildpath}/konversation-cli.jar"
