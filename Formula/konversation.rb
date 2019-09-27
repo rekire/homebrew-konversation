8d01a61b2fe8e2d3ea48649230da70d666cc709e
rekire <git@reki.re>
2019-09-27 14:27:41 +0200

    Correct hash

--- Formula/konversation.rb
+++ Formula/konversation.rb
@@ -2,7 +2,7 @@ class Konversation < Formula
   desc "Konversation is a tool to generate rich and diversified responses to the user of a voice application."
   homepage "https://github.com/rewe-digital-incubator/Konversation/"
   url "https://github.com/rewe-digital-incubator/Konversation/releases/download/cli-1.1.0-rc5/konversation-cli.jar"
-  sha256 "9eab651e90aae12cb001517cf0cfc77a9638ae3d21056dd36993f127e370a2a0"
+  sha256 "47fb618df62a3d905b39d2501a07b01e71918f5387457e1125ac871cc60ab62a"
 
   def install
     libexec.install "#{buildpath}/konversation-cli.jar"
