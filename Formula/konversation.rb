7058402d090d20a9c383a0239988a36ca022f37c
rekire <git@reki.re>
2019-10-01 12:31:24 +0200

    Update homebrew to version 1.1.0-rc8

--- Formula/konversation.rb
+++ Formula/konversation.rb
@@ -1,8 +1,8 @@
 class Konversation < Formula
   desc "Konversation is a tool to generate rich and diversified responses to the user of a voice application."
   homepage "https://github.com/rewe-digital-incubator/Konversation/"
-  url "https://github.com/rewe-digital-incubator/Konversation/releases/download/cli-1.1.0-rc7/konversation-cli.jar"
-  sha256 "f8dd490e5048c99d69ae01e665fe3a16209d9351a4fd7be8884844278da43c81"
+  url "https://github.com/rewe-digital-incubator/Konversation/releases/download/cli-1.1.0-rc8/konversation-cli.jar"
+  sha256 "17a90ada7a7287ce70f0c6140cebc01c0a2b8d935d982a574c18c56c3650d274"
 
   def install
     libexec.install "#{buildpath}/konversation-cli.jar"
