5782974a55165a62d5edb737e0112c99b71ed381
rekire <git@reki.re>
2019-10-04 11:38:06 +0200

    Update konversation.rb

--- Formula/konversation.rb
+++ Formula/konversation.rb
@@ -3,6 +3,7 @@ class Konversation < Formula
   homepage "https://github.com/rewe-digital-incubator/Konversation/"
   url "https://github.com/rewe-digital-incubator/Konversation/releases/download/cli-1.1.0-rc9/konversation-cli.jar"
   sha256 "e5e625111ead719449afaed5cb1a4d12726f9199a57e9d8e1458c794b7fe9159"
+  version "1.1.0-rc9"
 
   def install
     libexec.install "#{buildpath}/konversation-cli.jar"
