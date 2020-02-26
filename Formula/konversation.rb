3480d803036ccf819eb38e7a71c80fdf64148c92
rekire <git@reki.re>
2020-02-26 09:38:15 +0100

    Update repo url

--- Formula/konversation.rb
+++ Formula/konversation.rb
@@ -1,7 +1,7 @@
 class Konversation < Formula
   desc "Konversation is a tool to generate rich and diversified responses to the user of a voice application."
-  homepage "https://github.com/rewe-digital-incubator/Konversation/"
-  url "https://github.com/rewe-digital-incubator/Konversation/releases/download/cli-1.1.0/konversation-cli.jar"
+  homepage "https://github.com/rewe-digital/Konversation/"
+  url "https://github.com/rewe-digital/Konversation/releases/download/cli-1.1.0/konversation-cli.jar"
   sha256 "582f71492d59fa5823dd16ff59c351e54f279ef5db96ea569183d4a4b3edcaa3"
   version "1.1.0"
 
